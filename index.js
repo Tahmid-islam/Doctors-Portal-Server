0d6ffe03129cb039215d30862cad68044d3e4f28
Md. Ahanaf Tahmid Islame <tahmid231@gmail.com>
2021-11-16 21:31:16 +0600

    update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
