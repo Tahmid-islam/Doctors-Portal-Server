64841c5eef6b79c8b1391b32d8f4051c00b57a28
Md. Ahanaf Tahmid Islame <tahmid231@gmail.com>
2021-11-16 21:20:10 +0600

    update

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const ObjectId = require("mongodb").ObjectId;
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const stripe = require("stripe")(
+  sk_test_51IgBWFJnP3U4GjEn3c9mOk3zkK8gBxmLiQHssOvcKxAVsxe0UZkjoU1lfSlu1xqa9jzaOALDXfWhaddkrQgiyX8d00sA6hQz73
+);
 const fileUpload = require("express-fileUpload");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: "doctors-portal-9b37a",
+    private_key_id: "6a9b585f0efda547eca5023f4b8fc35558b1a98d",
+    private_key:
+      "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCd5gyVXBRQsZ5b\nicH/zh7dbHLYD3/rnwjxuEjBWKRCtbyFu/3UnnbJWlgGeqdpioswgUDzZ/Bf4TN9\nMn8jTD41nvNXUFOTkXuutltJAmIj7geXkg+rvxhQJjgP2EVPOZil3gH+jejCjzFR\n2/gm3d5M5mwNdNOK6eaB7I7Op+Zy3vIY+D/2nLmAHMNDxTLJHtjSIOC+z1r/5uJ+\nBKemsYt58rxcKkLzGMxuVz5lKV+r+XgqDFWyk8Xky9roUhD9ehmjx6o0UFBJ6BOh\n+jnTj7UHdNU5pIa4dsMXF74AL4DkWmiP3sGN9vWsPVhUWl2hjwPCjHdphHNTWZta\nVPHmtKNvAgMBAAECggEAAu9X6YjPdIaF/9k/gA6Sl/a1lcWH+6xg7AP1uj5NuKhh\ny4c92xoM8YYv362fogBqFj84zVgaaYspPAOtxaKw/1R9MnDISj8TMkv/xgt4y9vX\nS/PHkx+CB1oGgxONNOvy+0D1VDLKvPRmFsmaGGLZsx/kqfAIz0AKFv0mKbozjYwG\n2iWT4k3mCTuoGxiqxpj2ug5L3U/FQqZHZJTPiXKcFltDdZS+5rv/oFBEMPfOaARv\nbgAu4qFc/MCiLZEBGi/XLUa0fYz4qdvfQcLAKXdpB6lE5z8LybMVrfhQrohSibON\nD7Ty2AHSgLghuYYZaVqjzhhCPflQYWIlnReXHAWOoQKBgQDRAAW26P5dLzzXAB0r\nbCroD3LmvXe7OmTx6+SyNfwllhYCpXIKYWVLDydLjjyWcv5Raoo69Q5I1hjI2O9K\nTOOxDziZgt6o5m2GpWpDszgUOa2mKmUgeIWEQ0elOcfmLLDIVCssZex6TDe3rVIv\n/3cQcMLuqSdanEifdIhhGDwpoQKBgQDBaCeFrjhmhAGUwL6TsF4slF8L2HB6GeAU\nL5dD4LBP3cOdsO6wZRmZMYlv74xaTKneZjR6MD9F5zxQd9zxqSOfymrl6JTzCz4S\nnYCIEPN03feKTBjmQC3mTpPkMvJ//PWBbUKdiWlUEWWb0kFRcFjIrezotPf5XGpD\nnUD7gU9TDwKBgE3ogjjS1a8K4tUKl9UN91iWGbDlw/IpLGP6GUBNW3bvYbkOz1oL\nQGUgevdxSCiVKTRUUL71hqOkG40TQPAjhWUFVYX0AOhZLU7Z7qjZ7/eqII8mkpfu\nFWdeZSC0T04ALs4eghGE1QgpzUNM+qwlwM6CGkg5ChB+3ZuDJyt6i0LBAoGBAIDo\nS9dU7LCfcXJVGXDUj6BE8Ci0BRvM3UZ3Beou+zTwSW50PFMDv5EUrFcYIcMZ7+qU\n0otSVwnfeprjmTbVFjJtttwwLf7kUmYT5lRKgAo328Kov7vPsQgc/YOO1YSq9HNk\ngI8BxFtNER1PgUunhvWSvyHHZLQmfYk9ac94O4fPAoGBAKs/PnUNxOha/VaBk6k9\nUXgjdyaJlYyVkMvZ3aE2hYOJypuJoOrNfVKe7GSwjjaI2NUT/kgX2a7xFfonbJch\nDZZ2eM2iQUxPcoEcUvm82GCq80d+UbYP7TT10j7cX0/nmgVxudL0j8xWP0AbTU9Y\n6R8RXHrWWnIpshNuzlZinb3f\n-----END PRIVATE KEY-----\n",
+    client_email:
+      "firebase-adminsdk-2lxyr@doctors-portal-9b37a.iam.gserviceaccount.com",
+    client_id: "107402487256959249853",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2lxyr%40doctors-portal-9b37a.iam.gserviceaccount.com",
+  }),
 });
 
 // middleware
@@ -20,7 +36,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://doctorDB:jfpAcuNOVPqXwIec@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
